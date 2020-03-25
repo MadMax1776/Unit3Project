ac64ee74266606affc1e4786f3fa0c2844308a83
mmeacham <maxwmeacham@gmail.com>
2020-03-25 13:23:30 -0400

    added auth code

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,45 @@ const app = angular.module('WonderApp', []);
 
  app.controller('MyController',['$http', function($http){
   this.name = null;
+  this.loggedInUser = false;
+  ////changing between true and false determines what will be on the page.
+  this.indexOfEditFormToShow = null;
 
 const controller = this;
+
+  this.signup = function(){
+    $http({
+      url: '/users',
+      method: 'POST',
+      data: {
+        username: this.signupUsername,
+        password: this.signupPassword
+      }
+    }).then(function(response){
+     controller.loggedInUser = response.data;
+   })
+  }
+
+  this.login = function(){
+        $http({
+            url:'/session',
+            method:'POST',
+            data: {
+                username: this.loginUsername,
+                password: this.loginPassword
+            }
+        }).then(function(response){
+            if(response.data.username){
+                controller.loggedInUser = response.data;
+            } else {
+                controller.loginUsername = null;
+                controller.loginPassword = null;
+                controller.signupUsername = null;
+
+                ////these two lines clear out the form if it's wrong
+            }
+        })
+      }
 
   this.createWonder = function() {
       $http({
@@ -62,6 +99,29 @@ function(error){
 }
 );
 };
+
+this.logout = function(){
+$http({
+    url:'/session',
+    method:'DELETE'
+}).then(function(){
+    controller.loggedInUser = false;
+    controller.loginUsername = null;
+    controller.loginPassword = null;
+    controller.signupPassword =null;
+
+})
+}
+
+$http({
+  method:'GET',
+  url:'/session'
+}).then(function(response){
+  // console.log(response);
+  if(response.data.username){
+  controller.loggedInUser = response.data
+  }
+});
 
 
 this.getWonder();
